00f9677d58056055d4597ff386bc298f41d88105
toxicalchemist <141489794+toxicalchemist@users.noreply.github.com>
2024-01-03 17:37:32 +0800

    Add files via upload

---
+++ VoteMate Voting System/Admin/INC/footer.php
@@ -0,0 +1,17 @@
+
+<div class="row bg-black text-center text-white">
+        <div class="col-12 my-3">
+           
+                Developed by VotemateResearchers<br>
+                For Research Purposes<br>
+                2023
+            </p>
+        </div>
+    </div>
+
+</div>
+
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+</body>
+</html>
